d5c8759fb4fc0c0f901a4ecdd480d64a9b80ecde
UJJWALTHAKUR28 <ujjwalthakur.008reena@gmail.com>
2026-04-30 11:54:29 +0530

    feat: update server initialization to use redis ping and add queue worker handling

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,13 @@ import app from "./app";
 import { prisma } from "./db/prisma";
 import { redis } from "./config/redis";
 const PORT = process.env.PORT || 3000;
+import { startPublishWorker,gracefulShutdown } from './queue';
 
 async function startServer() {
   try {
     await prisma.$connect();
     console.log('Connected to Prisma database');
-    await redis.connect();
+    await redis.ping();
     console.log('Connected to Redis');
     app.listen(PORT,() => {
   console.log(`Server is running on port ${PORT}`);
@@ -17,4 +18,9 @@ async function startServer() {
   catch(error){
     console.error('Error starting server:', error);
   }}
+const worker = startPublishWorker();
+process.on('SIGTERM', async () => {
+  await gracefulShutdown(worker);
+  process.exit(0);
+});
 startServer();
